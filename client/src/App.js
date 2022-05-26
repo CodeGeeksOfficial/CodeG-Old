9f3d6b10ace804ca8f12864d82101e10d842d66a
seelam Namrata <namrata9901seelam@gmail.com>
2022-05-26 15:30:13 +0530

    mainbox added

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 // import './App.css';
 import {Header} from './Components/Header';
 import Footer from './Components/Footer';
+import Mainbox from './Components/Mainbox';
 
 function App() {
   return (
     <>
       <div className="App">
       <Header/>
+      <Mainbox/> 
       <Footer/>
       </div>
     </>
