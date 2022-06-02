b72f5a7294949bc0fe8cb937f09ca26f88b7580a
Vipul <vmbansal2001@gmail.com>
2022-06-02 18:15:44 +0530

    Added bottom curve to homepage

--- client/src/Components/BecomeCodie.js
+++ client/src/Components/BecomeCodie.js
@@ -1,40 +1,42 @@
 import React from 'react'
 import './BecomeCodie.css'
-import Illustration from '../Assets/Become-Codie-Illustration.png' 
+import Illustration from '../Assets/Become-Codie-Illustration.png'
 import DiscordLogo from '../Assets/Discord-Bubble-Color.png'
 import TelegramLogo from '../Assets/Telegram-Logo.png'
 
 export default function BecomeCodie() {
   return (
-    <div className="become-codie-card">
-      <div className="become-codie-card-content">
-        <span>Become A Codie Now !</span>
-        <div className="become-codie-card-description">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti corporis nihil quas, fuga temporibus, veniam vero pariatur molestias quo totam illo recusandae incidunt iure mollitia eos magni! Aut, alias necessitatibus.
-        </div>
-        <div className="become-codie-links">
-          <div className="become-codie-discord-link">
-            <img src={DiscordLogo} alt="Discord Logo" />
-            <div className="join-us-at-discord">
-              <div className="text">
-              Join us @ <br/>
+    <div className="become-a-codie-curved-bg">
+      <div className="become-codie-card">
+        <div className="become-codie-card-content">
+          <span>Become A Codie Now !</span>
+          <div className="become-codie-card-description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti corporis nihil quas, fuga temporibus, veniam vero pariatur molestias quo totam illo recusandae incidunt iure mollitia eos magni! Aut, alias necessitatibus.
+          </div>
+          <div className="become-codie-links">
+            <div className="become-codie-discord-link">
+              <img src={DiscordLogo} alt="Discord Logo" />
+              <div className="join-us-at-discord">
+                <div className="text">
+                  Join us @ <br />
+                </div>
+                <span>Discord</span>
               </div>
-              <span>Discord</span>
+            </div>
+            <div className="become-codie-telegram-link">
+              <img src={TelegramLogo} alt="Telegram Logo" />
+              <div className="join-us-at-telegram">
+                <div className="text">
+                  Join us @ <br />
+                </div>
+                <span>Telegram</span>
+              </div>
             </div>
           </div>
-          <div className="become-codie-telegram-link">
-            <img src={TelegramLogo} alt="Telegram Logo" />
-            <div className="join-us-at-telegram">
-            <div className="text">
-              Join us @ <br/>
-              </div>
-              <span>Telegram</span>
-            </div>
-          </div>
         </div>
-      </div>
-      <div className="become-codie-card-illustration">
+        <div className="become-codie-card-illustration">
           <img src={Illustration} alt="become a codie" />
+        </div>
       </div>
     </div>
   )
