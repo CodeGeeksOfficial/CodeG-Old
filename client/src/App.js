2189604a9f0b0a2894dc0d4d68f908c5f9f45f54
Sandeep Jha <91358961+ft-jsandeep@users.noreply.github.com>
2022-06-04 11:21:41 +0530

    tailwind installation (currently not working)

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import './App.css';
-import Navbar from './Components/Navbar';
-import CreateContestCard from './Components/CreateContest';
-import Footer from './Components/Footer';
-import Mainbox from './Components/Mainbox';
-import BecomeCodie from './Components/BecomeCodie';
+import React from "react";
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import CreateContestCard from "./Components/CreateContest";
+import Footer from "./Components/Footer";
+import Mainbox from "./Components/Mainbox";
+import BecomeCodie from "./Components/BecomeCodie";
+import SignUp from "./pages/SignUp";
 
 function App() {
   return (
     // <Mainbox/>
     <div className="App">
-      <Navbar/>
+      <Navbar />
       {/* <Mainbox/> */}
       <section className="create-contest-card-background">
-        <CreateContestCard/>
+        <CreateContestCard />
       </section>
       <section className="become-codie-card-background">
-        <BecomeCodie/>
+        <BecomeCodie />
       </section>
-      <Footer/>
+      <SignUp />
+      <Footer />
     </div>
   );
 }
---
+++ client/src/pages/SignUp.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const SignUp = () => {
+  return (
+    <h1 className="px-3 text-3xl font-bold underline text-white">Signup</h1>
+  );
+};
+
+export default SignUp;
